36d71ce4ceb9444c55f5f89b59ab39a5cf87eccc
hansol <hansolnoh95@gmail.com>
2021-07-01 12:49:45 +0900

    :construction: mapview layout 구현 중

--- CA-PIN_IOS/CA-PIN_IOS/Screens/Map/ViewControllers/MapViewController.swift
+++ CA-PIN_IOS/CA-PIN_IOS/Screens/Map/ViewControllers/MapViewController.swift
@@ -8,14 +8,69 @@
 import UIKit
 
 import NMapsMap
+import SnapKit
+import Then
 
+// MARK: - MapViewController
 class MapViewController: UIViewController {
   
+  // MARK: - Components
+  let topView = UIView()
+  let titleLabel = UILabel()
+  let menuButton = UIButton()
+  let mapView = NMFMapView(frame: .zero)
+  let toggleView = UIView()
+  let capinMapButton = UIButton()
+  let myMapButton = UIButton()
+  
+  // MARK: - LifeCycles
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    let mapView = NMFMapView(frame: view.frame)
-        view.addSubview(mapView)
+    self.navigationController?.navigationBar.isHidden = true
   }
   
+}
+
+// MARK: - Extensions
+extension MapViewController {
+  // MARK: - Layout Helpers
+  func layout() {
+    layoutTopView()
+    layoutTitleLabel()
+    layoutMenuButton()
+    layoutMapView()
+    layoutToggleView()
+    layoutCapinMapButton()
+    layoutMyMapButton()
+  }
+  func layoutTopView() {
+    view.add(topView) {
+      $0.backgroundColor = .white
+      $0.cornerRadius = 10
+      $0.snp.makeConstraints {
+        $0.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
+        $0.leading.equalTo(self.view.safeAreaLayoutGuide.snp.leading)
+        $0.trailing.equalTo(self.view.safeAreaLayoutGuide.snp.trailing)
+        $0.height.equalTo(146)
+      }
+    }
+  }
+  func layoutTitleLabel() {
+    
+  }
+  func layoutMenuButton() {
+    
+  }
+  func layoutMapView() {
+    
+  }
+  func layoutToggleView() {
+    
+  }
+  func layoutCapinMapButton() {
+    
+  }
+  func layoutMyMapButton() {
+    
+  }
 }
