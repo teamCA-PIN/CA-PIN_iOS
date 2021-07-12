2e945435a15a4855a94689b924e8446864fef7a2
seohyun-106 <changseohyun@sookmyung.ac.kr>
2021-07-13 00:09:23 +0900

    🚧 카테고리 수정 뷰 ing~

--- CA-PIN_IOS/CA-PIN_IOS/Screens/TabbarViewController.swift
+++ CA-PIN_IOS/CA-PIN_IOS/Screens/TabbarViewController.swift
@@ -35,7 +35,7 @@ extension TabbarViewController {
     secondNavigationController.addChild(secondTabController)
     
     let thirdNavigationController = UINavigationController()
-    let thirdTabController = LoginViewController()
+    let thirdTabController = EditCategoryViewController()
     thirdNavigationController.addChild(thirdTabController)
     
     
