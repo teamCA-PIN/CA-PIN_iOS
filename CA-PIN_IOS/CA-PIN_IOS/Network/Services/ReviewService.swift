fafa3bc925f93a61e27b4b7cdaafce6895b93618
hansolnoh95 <hansolnoh95@naver.com>
2021-07-16 22:43:47 +0900

    :art: userservice 수정

--- CA-PIN_IOS/CA-PIN_IOS/Network/Services/ReviewService.swift
+++ CA-PIN_IOS/CA-PIN_IOS/Network/Services/ReviewService.swift
@@ -38,7 +38,6 @@ extension ReviewService: TargetType {
       return "/reviews"
     case .writeReview:
       return ""
-    case .editReview(let reviewId):
     case .editReview(let reviewId, _, _, _, _, _):
       return "/reviews/\(reviewId)"
     case .deleteReview(let reviewId):
